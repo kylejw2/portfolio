0f2c8ccf83a5bfb000d2e70b79c20a1508169b51
Kyle Wright <kylejamwright@gmail.com>
2020-09-18 12:25:54 -0600

    Finished title animation

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -4,22 +4,24 @@ import { useSpring, animated } from 'react-spring';
 
 const Letter = (props) => {
 
-    const [answer, setAnswer] = useState(false)
+    const [answer, setAnswer] = useState(false);
+    const [clicked, setClicked] = useState(false)
 
-    const accepted = Math.floor(Math.random() * 10) === 0 ? true : false;
+    const accepted = Math.floor(Math.random() * 2) === 0 ? true : false;
     const fade = useSpring({
         config: {
-            mass: 10
+            mass: 8
         },
         from: {
-            transform: accepted ? 'translate3d(0px,200px,0) scale(1) rotateX(0deg)' : 'translate3d(0px,-200px,0) scale(1) rotateX(0deg)',
+            transform: accepted ? 'translate3d(0px,100px,0) scale(1) rotateX(0deg)' : 'translate3d(0px,-100px,0) scale(1) rotateX(0deg)',
         },
         transform: answer ? 'translate3d(0px,-20px,0px) scale(1) rotateX(0deg)' : 'translate3d(0px,0,0) scale(1) rotateX(0deg)',
         fontFamily: answer ? "Bubblegum Sans" : "Londrina Shadow",
+        display: clicked ? 'none' : ''
     });
 
     return (
-        <animated.h1 className='title' onMouseEnter={() => setAnswer(true)} onMouseLeave={() => setAnswer(false)} style={fade}>{props.letter}</animated.h1>
+        <animated.h1 className='title' onClick={() => setClicked(true)} onMouseEnter={() => setAnswer(true)} onMouseLeave={() => setAnswer(false)} style={fade}>{props.letter}</animated.h1>
     )
 }
 
