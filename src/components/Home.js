f894fae6fb31207a3be531275defb2e4f8cac342
Kyle Wright <kylejamwright@gmail.com>
2020-09-17 20:14:02 -0600

    Working on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,20 +30,21 @@ const Home = () => {
 
     const [color, setColor] = useState(colors[0]);
     const [tile, setTile] = useState(tiles[0]);
-    let index = 0;
+    const [index, setIndex] = useState(0);
+    // let index = 0;
 
     useEffect(() => {
         const interval = setInterval(() => {
-            if (index === colors.length - 1) {index = 0}
-            else {index++}
+            if (index === colors.length - 1) {setIndex(0)}
+            else {setIndex(index => index + 1)}
             setColor(colors[index]);
             setTile(tiles[index]);
-
+            console.log(index);
             // make each letter jump in through a position at -30px
 
-        }, 7000);
+        }, 2000);
         return () => clearInterval(interval);
-      }, []);
+      }, [index, color, tile]);
 
     const fade = useSpring({
         background: color
@@ -55,21 +56,22 @@ const Home = () => {
             className={i === index ? 'buttons fill-center': 'buttons'}
             onClick={() => { 
                 setTile(tiles[i]); 
-                setColor(colors[i])
+                setColor(colors[i]);
+                setIndex(i);
             }}></button>);
     }
 
     const getTitle = () => {
         const letters = [];
         for (let i = 0; i < tile.title.length; i++) {
-            const font = i % 2 === 0 ? 'Monoton' : '';
+            const font = i % 2 === 0 ? 'Londrina Shadow' : '';
             
             if (tile.title[i] === ' ') {letters.push(<span key={`${index}.${i}`} style={{margin: '0 8px'}}></span>)}
             else {letters.push(<animated.h1 key={`${index}.${i}`} style={{fontFamily: font}} className='title'>{tile.title[i]}</animated.h1>) }
         }
         return letters;
     }
-//font-family: 'Press Start 2P', cursive;
+
     return (
 
         <main className='home'>
@@ -79,9 +81,10 @@ const Home = () => {
                 <div className='tile-title'>
                     {getTitle()}
                 </div>
-                
+
                 <h3 className='description'>{tile.description}</h3>
-                <button className='learn-more' style={{color: color}}>Learn More</button>
+                {index !== 0 ? <button className='learn-more' style={{color: color}}>Learn More</button> : ''}
+                
             </section>
             <div className='button-group'>
                 {getButtons()}
